9271949ef2cc7c3c01059c0e2ccfc23d5a460942
laura-schoenherr <lauraschoenherr@outlook.de>
2022-01-17 19:46:39 +0100

    add keycloak files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import authentication from "@/plugins/authentication"
 
 Vue.config.productionTip = false
+Vue.use(authentication)
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.$keycloak
+    .init({ onLoad: 'login-required', checkLoginIframe: false })
+    .then(() => {
+      new Vue({
+        router,
+        render: h => h(App)
+      }).$mount('#app')
+    })
+
